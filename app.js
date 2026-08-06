8a7999e32cff60caf81119c0969785a8509c4a91
Sanil <patwasanil39@gmail.com>
2026-08-06 06:37:13 +0530

    new commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
--- init/index.js
+++ init/index.js
@@ -1,3 +1,6 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
@@ -11,7 +14,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect("mongodb+srv://SanilPatwa:2KtlLAzBcnlHDkxZ@cluster0.mgxmxcc.mongodb.net/wanderlust?retryWrites=true&w=majority");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
